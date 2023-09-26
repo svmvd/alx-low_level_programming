d53145ee728a3e431fdf7f661e9db5ca2b6ccfdf
svmvd <ABDESSAMADWILLIAM@GMAIL.COM>
2023-09-26 07:32:52 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -9,6 +10,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *);
 char *leet(char *);
-int _putchar(char c);
+void print_number(int n);
 
 #endif
